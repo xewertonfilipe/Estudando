6685c71b1990d3e4db8d9bb7f1cb23aa9333489e
Ewerton Filipe <xewertonfilipe@gmail.com>
2018-05-02 22:24:38 -0300

    Função de validação do cliente

--- JavaScript/js/form.js
+++ JavaScript/js/form.js
@@ -1,19 +1,46 @@
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event){
     event.preventDefault();
-
     var form = document.querySelector("#form-adiciona");
-
     var paciente = obtemPacienteDoFomulario(form);
-
     var pacienteTr = montaTr(paciente);
-
     var tabela = document.querySelector("#tabela-pacientes");
+
+    var erros = validaPaciente(paciente);
+
+    if(erros.length > 0){
+        exibeMensagensDeErro(erros);
+        return;
+    }
+
     tabela.appendChild(pacienteTr);
 
     form.reset();
+    var mensagensErros = document.querySelector("#mensagens-erro");
+    mensagensErros.innerHTML = "";
+
 
 });
+
+function validaPaciente(paciente){
+
+    var erros = [];
+
+    if(paciente.nome.length == 0) erros.push("O nome não pode ser em branco.");
+
+    if(paciente.gordura.length == 0) erros.push("A gardura não pode ser em branco.");
+
+    if(paciente.peso.length == 0) erros.push("O peso não pode ser em branco.");
+
+    if(paciente.altura.length == 0) erros.push("A altura não pode ser em branco.");
+
+    if(!validaPeso(paciente.peso)) erros.push("Peso é inválido");
+
+    if(!validaAltura(paciente.altura)) erros.push("Altura inválida");
+
+    return erros;
+
+}
 
 function obtemPacienteDoFomulario(form){
 
@@ -51,4 +78,16 @@ function montaTr(paciente){
     pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
 
     return pacienteTr;
+}
+
+//Cria uma li dentro da ul para cada erro encontrado.
+function exibeMensagensDeErro(erros){
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro){
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
 }
